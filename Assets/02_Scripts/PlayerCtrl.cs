1a1162df438e0822878b786d6c481e19dea0eb6e
EhyunHwaNyeong <dltjrrb4321@gmail.com>
2025-08-29 14:17:58 +0900

    41 241

--- Assets/02_Scripts/PlayerCtrl.cs
+++ Assets/02_Scripts/PlayerCtrl.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    [SerializeField] Transform tr;
-    [SerializeField] float moveSpeed = 10.0f;
+    const float TIME_INTER = 0.25f;
+    const float INPUT_VALUE = 0.1f;
+    Transform tr;
+    Animation anime;
+    [SerializeField] float moveSpeed = 5.0f;
     [SerializeField] float turnSpeed = 360.0f;
     void Start()
     {
         tr = GetComponent<Transform>();
-        
+        anime = GetComponent<Animation>();
+
+        anime.Play("Idle");
     }
 
     void Update()
@@ -18,11 +23,39 @@ public class PlayerCtrl : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         float r = Input.GetAxis("Mouse X");
+
         // Transform.position
         // transform.position += new Vector3(0, 0, 1);
         // normalized vector
+
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
         tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
+
+        PlayerAnime(h, v);
     }
+
+    void PlayerAnime(float h, float v)
+    {
+        if (v >= INPUT_VALUE)
+        {
+            anime.CrossFade("RunF", TIME_INTER);
+        }
+        else if (v <= -INPUT_VALUE)
+        {
+            anime.CrossFade("RunB", TIME_INTER);
+        }
+        else if (h >= INPUT_VALUE)
+        {
+            anime.CrossFade("RunR", TIME_INTER);
+        }
+        else if (h < -INPUT_VALUE)
+        {
+            anime.CrossFade("RunL", TIME_INTER);
+        }
+        else
+        {
+            anime.CrossFade("Idle", TIME_INTER);
+        }
+    }   
 }
