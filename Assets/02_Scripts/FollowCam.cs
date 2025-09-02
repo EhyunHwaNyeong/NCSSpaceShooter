da07f43913fe84637cd0f01723f8ba6263095bc9
EhyunHwaNyeong <dltjrrb4321@gmail.com>
2025-09-02 13:49:30 +0900

    4 124

---
+++ Assets/02_Scripts/FollowCam.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform targetTr;
+    Transform camTr;
+
+    [Range(2.0f, 20.0f)] public float distance = 10.0f;
+    [Range(0.0f, 10.0f)] public float height = 2.0f;
+    public float damping = 10.0f;
+    public float targetOffset;
+    Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        camTr = GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        Vector3 pos = targetTr.position
+                    + (-targetTr.forward * distance)
+                    + (Vector3.up * height);
+        // camTr.position = Vector3.Slerp
+        //             (camTr.position, pos, Time.deltaTime * damping);
+        camTr.position = Vector3.SmoothDamp
+                        (camTr.position, pos, ref velocity, damping);
+        camTr.LookAt(targetTr.position + (targetTr.up * targetOffset));
+    }
+}
